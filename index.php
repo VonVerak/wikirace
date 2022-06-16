4e4993cfefcbc5281c1a377da84e3c0c61fca5ed
Trevor Osborne <thetrevorosborne@gmail.com>
2022-06-15 21:13:49 -0600

    This is a test

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
-// Here is some code that is useless
+<!doctype html>
 
-<p>hello</p>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-// Here is some more testing
+    <title>Wiki Race</title>
+    <meta name="description" content="Alpha - Wiki Race">
+    <meta name="author" content="VonVerak">
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+<iframe src="https://en.wikipedia.org/wiki/Odd-eyed_cat" title="description" height="800" width="800" onLoad="alert(this.contentWindow.location);"></iframe>
+
+<p id="test">-1</p>
+
+<script>
+    function alert(iframe)
+    {
+        current = parseInt(document.getElementById("test").innerHTML)
+        document.getElementById("test").innerHTML = current + 1;
+    }
+</script>
+
+</body>
+</html>
